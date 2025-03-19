9b7e9738b63aca0297618b7f6671c4e4403bfa9e
McCamble Kofi Fordjour Bobie <fordjourbobiem@gmail.com>
2025-03-19 16:37:26 +0000

    accounts

--- app/landlord/listings/edit/[id]/page.tsx
+++ app/landlord/listings/edit/[id]/page.tsx
@@ -1,13 +1,20 @@
 import LandlordHeader from "@/components/landlord-header"
 import EditListingForm from "@/components/edit-listing-form"
 
-export default function EditListingPage({ params }: { params: { id: string } }) {
+
+
+export default async function EditListingPage({
+    params,
+  }: {
+    params: Promise<{ slug: string }>
+  }) {
+    const { slug } = await params
   return (
     <div className="min-h-screen flex flex-col">
       <LandlordHeader />
       <div className="container py-6 flex-1">
         <h1 className="text-3xl font-bold mb-6">Edit Listing</h1>
-        <EditListingForm id={params.id} />
+        <EditListingForm id={slug} />
       </div>
     </div>
   )
