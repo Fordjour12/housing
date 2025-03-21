7f5d64696f7ce559d73ec908d7f7e4eb980e6769
McCamble Kofi Fordjour Bobie <fordjourbobiem@gmail.com>
2025-03-21 19:06:51 +0000

    login

--- components/auth/register.tsx
+++ components/auth/register.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -13,7 +12,6 @@ import {
 	FormItem,
 	FormLabel,
 	FormControl,
-	FormDescription,
 	FormMessage,
 } from "@/components/ui/form";
 import Image from "next/image";
